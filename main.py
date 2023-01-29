ee84c3860b89d04cf0998b724ed0a1654e060a9d
Uoiferise <ws555w@mail.ru>
2023-01-29 21:21:23 +0300

    update

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from resources import ResourceManager
 def main():
     data = ResourceManager.get_data(report_name='input_data/titanium_base/titanium_base_info.xlsx')
 
+
 if __name__ == '__main__':
     main()
